d2439830824d7320ffd96df832449e0e5517bb91
Skshm <eminate894567@gmail.com>
2021-05-26 10:24:46 +0530

    Push

--- Termux_Setup.sh
+++ Termux_Setup.sh
@@ -11,21 +11,18 @@ else
 pkg update && pkg update -y
 
 ## * Termux access to global storage
-pkg install nnn wget zsh ffmpeg nano -y
-pkg install python termux-exec -y
+pkg install nnn wget zsh ffmpeg nano nodejs python termux-exec -y
 pip install youtube-dl -U
 
 #? Settingup the .zshrc
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-cp /data/data/com.termux/files/home/.oh-my-zsh > nzshrc
-sed '10c\'"Set ZSH_THEME="powerlevel10k/powerlevel10k" nzshrc > /data/data/com.termux/files/home/.oh-my-zsh
-echo "alias Dat='export PATH="$PATH:/data/data/com.termux/files/home/.dat/releases/dat-14.0.2-linux-x64"' >> .zshrc
+cp .zshrc > nzshrc
+sed '10c\'""ZSH_THEME="powerlevel10k/powerlevel10k"" nzshrc > .zshrc
+echo "alias Dat= ".dat/releases/dat-14.0.2-linux-x64/dat"" >> .zshrc
 rm nzshrc
 
 #! Dat function
-wget -qO- https://raw.githubusercontent.com/datproject/dat/master/download.sh | bash
-export PATH="$PATH:/data/data/com.termux/files/home/.dat/releases/dat-14.0.2-linux-x64"
-
+npm install -g dat
 
 ## * Settingup ZSH
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
